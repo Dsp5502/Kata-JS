f01be0dfdc5cbc940fdd0344ad4bf5824edd9464
David Puerto Guerrero <dsp5502@gmail.com>
2023-07-08 07:59:31 -0500

    Add validacion password

--- CajeroAutomático/script.js
+++ CajeroAutomático/script.js
@@ -1,16 +1,62 @@
 const consulta = document.getElementById('consulta');
 const ingresar = document.getElementById('ingresar');
+const login = document.getElementById('login');
 const monto = document.getElementById('monto');
 const nombreCuenta = document.getElementById('nombre');
 const retirar = document.getElementById('retirar');
 const saldo = document.getElementById('saldo');
 const select = document.getElementById('clientes');
+const password = document.getElementById('password');
+const modal = document.querySelector('dialog');
+const msg = document.getElementById('msg');
 
 const cuentas = [
-  { nombre: 'Mali', saldo: 200 },
-  { nombre: 'Gera', saldo: 290 },
-  { nombre: 'Maui', saldo: 67 },
+  { nombre: 'Mali', saldo: 200, password: 1234, login: false },
+  { nombre: 'Gera', saldo: 290, password: 4586, login: false },
+  { nombre: 'Maui', saldo: 67, password: 7868, login: false },
 ];
+
+const guardarCuentas = () => {
+  const cuentasSinPassword = cuentas.map((cuenta) => {
+    return {
+      nombre: cuenta.nombre,
+      saldo: cuenta.saldo,
+    };
+  });
+  localStorage.setItem('cuentas', JSON.stringify(cuentasSinPassword));
+};
+
+const cargarCuentas = () => {
+  const cuentasGuardadas = JSON.parse(localStorage.getItem('cuentas'));
+  if (cuentasGuardadas) {
+    cuentasGuardadas.forEach((cuentaGuardada) => {
+      const cuenta = cuentas.find(
+        (cuenta) => cuenta.nombre === cuentaGuardada.nombre
+      );
+      cuenta.saldo = cuentaGuardada.saldo;
+      cuenta.login = cuentaGuardada.login;
+    });
+  }
+};
+
+cargarCuentas();
+
+const validarPassword = () => {
+  const nombre = select.value;
+
+  const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
+  if (password.value !== '' && cuenta.password === Number(password.value)) {
+    cuenta.login = true;
+    password.disabled = true;
+    login.disabled = true;
+    login.innerText = 'Bienvenid@';
+    login.style.backgroundColor = 'gray';
+    mostrarModal('Bienvenid@');
+    return true;
+  }
+  mostrarModal('Ingresa una contraseña válida');
+  return false;
+};
 
 const mostrarSaldo = (nombre, respuesta = '') => {
   nombreCuenta.innerText = nombre;
@@ -20,6 +66,10 @@ const mostrarSaldo = (nombre, respuesta = '') => {
 
 const actualizarSaldo = (nombre, monto, mensaje) => {
   const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
+  if (!cuenta.login) {
+    mostrarModal('Tienes que ingresar tu contraseña');
+    return;
+  }
   cuenta.saldo += monto;
   mostrarSaldo(nombre, mensaje);
 };
@@ -27,6 +77,10 @@ const actualizarSaldo = (nombre, monto, mensaje) => {
 const consultar = () => {
   const nombre = select.value;
   const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
+  if (!cuenta.login) {
+    mostrarModal('Tienes que ingresar tu contraseña');
+    return;
+  }
   mostrarSaldo(nombre, `Tu saldo es: $${cuenta.saldo}`);
 };
 
@@ -34,12 +88,17 @@ const depositar = () => {
   const nombre = select.value;
   const montoIngresado = Number(monto.value);
 
+  const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
+  if (!cuenta.login) {
+    mostrarModal('Tienes que ingresar tu contraseña');
+    return;
+  }
+
   if (montoIngresado <= 0) {
     mostrarError('Tienes que ingresar una cantidad mayor a $0');
     return;
   }
 
-  const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
   const saldoActualizado = cuenta.saldo + montoIngresado;
 
   if (saldoActualizado > 990) {
@@ -47,24 +106,33 @@ const depositar = () => {
     return;
   }
 
+  monto.value = '';
+
   actualizarSaldo(
     nombre,
     montoIngresado,
     `Depósito de $${montoIngresado} realizado, tu nuevo saldo es: $${saldoActualizado}`
   );
+
+  guardarCuentas();
 };
 
 const retirarCantidad = () => {
   const nombre = select.value;
   const montoRetirado = Number(monto.value);
 
+  const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
+  const saldoActualizado = cuenta.saldo - montoRetirado;
+
+  if (!cuenta.login) {
+    mostrarModal('Tienes que ingresar tu contraseña');
+    return;
+  }
+
   if (montoRetirado <= 0) {
     mostrarError('Tienes que ingresar una cantidad mayor a $0');
     return;
   }
-
-  const cuenta = cuentas.find((cuenta) => cuenta.nombre === nombre);
-  const saldoActualizado = cuenta.saldo - montoRetirado;
 
   if (saldoActualizado < 10) {
     mostrarError('No puedes tener menos de $10 en tu cuenta');
@@ -76,11 +144,27 @@ const retirarCantidad = () => {
     return;
   }
 
+  monto.value = '';
+
   actualizarSaldo(
     nombre,
     -montoRetirado,
     `Retiro de $${montoRetirado} realizado, tu nuevo saldo es: $${saldoActualizado}`
   );
+
+  guardarCuentas();
+};
+
+const logout = () => {
+  const cuenta = cuentas.find((cuenta) => cuenta.nombre === select.value);
+  cuenta.login = false;
+  password.value = '';
+  password.disabled = false;
+  login.disabled = false;
+  login.innerText = 'Ingresar';
+  login.style.backgroundColor = '';
+  monto.value = '';
+  mostrarSaldo('', '');
 };
 
 const mostrarError = (mensaje) => {
@@ -88,7 +172,11 @@ const mostrarError = (mensaje) => {
   saldo.innerText = mensaje;
 };
 
-// Agregar las cuentas al select
+const mostrarModal = (mensaje) => {
+  modal.showModal();
+  msg.innerText = mensaje;
+};
+
 cuentas.forEach((cuenta) => {
   const option = document.createElement('option');
   option.value = cuenta.nombre;
@@ -98,4 +186,6 @@ cuentas.forEach((cuenta) => {
 
 consulta.addEventListener('click', consultar);
 ingresar.addEventListener('click', depositar);
+login.addEventListener('click', validarPassword);
 retirar.addEventListener('click', retirarCantidad);
+select.addEventListener('change', logout);
